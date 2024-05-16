bad1ee3a0a82444b37d75ceaefc75a8a4d3357bc
Pierre Vauterin <pivnx@cowi.com>
2024-05-16 10:37:09 +0530

    Changed progressbar colors

--- progressbar.py
+++ progressbar.py
@@ -1,6 +1,7 @@
 import customtkinter
 import os
 from fileHandling import extract_text_from_pdf
+import time
 
 def clicker():
     testProgress.step()
@@ -12,15 +13,15 @@ def createProgressBar(bot,keywords,date,search,textList,fileList):
     customtkinter.set_appearance_mode(bot.colorTheme[0])
     customtkinter.set_default_color_theme(bot.colorTheme[1])
     root=customtkinter.CTk()
-    root.title("Test progress bar")
-    root.geometry("400x200")
+    root.title("Files processing")
+    root.geometry("400x90")
     testProgress=customtkinter.CTkProgressBar(root,orientation="horizontal",
                                               width=300,height=20,corner_radius=100,mode="determinate",determinate_speed=.1,indeterminate_speed=.5,
-                                              border_width=.5,border_color="grey",fg_color=("black","white"),progress_color="blue")
-    testProgress.pack(pady=40)
+                                              border_width=.5,border_color="grey",fg_color=("black","white"),progress_color=("red","blue"))
+    testProgress.pack(pady=10)
     testProgress.set(0)
     theLabel=customtkinter.CTkLabel(root,text="",font=("Helvetica",18))
-    theLabel.pack(pady=10)
+    theLabel.pack()
     root.after(100,lambda:processFiles(testProgress, root, theLabel,bot,keywords,date,search,textList,fileList)) # Maybe time this
     root.protocol("WM_DELETE_WINDOW", lambda: onClosing(root))
     root.mainloop()
@@ -42,6 +43,7 @@ def processFiles(testProgress,root,label,bot,keywords,date,search,textList,fileL
             testProgress.set(progressStep)
             progressStep+=iterStep
             root.update_idletasks()
-            label.configure(text="Files processed: "+str(i)+"/"+str(bot.numberFiles))
+            label.configure(text="Files processed: "+str(i+1)+"/"+str(bot.numberFiles))
     testProgress.stop()
+    time.sleep(2)
     onClosing(root)
