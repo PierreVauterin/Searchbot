9a91d34470b17789d58423ff099d14e951e69b64
PierreVauterin <vauterinp@gmail.com>
2024-06-26 14:56:46 +0530

    No more error for protected files

--- fileHandling.py
+++ fileHandling.py
@@ -9,35 +9,39 @@ def extract_text_from_pdf(pdf_file_path:str,listKeywords:list[str],filename:str,
     res,outputCreated,outputModified,firstModification,pagesKeyword= "",False,False,True,[]
     outputExists=os.path.isfile(bot.output+"/BOT_"+bot.query+"_"+date+".pdf")
     if(outputExists):output=fitz.open(bot.output+"/BOT_"+bot.query+"_"+date+".pdf")
-    for page in doc:
-        page_text = page.get_text()
-        lines = page_text.split('\n')
-        sanitized_lines = [line for line in lines if not line.strip().isdigit()]
-        sanitized_text = '\n'.join(sanitized_lines)
-        res += sanitized_text
-        if(len(findKeyword(listKeywords,sanitized_text,search))!=0):
-            if(not outputExists):
-                output=fitz.open()
-                outputCreated=True
-                outputExists=True
-            if(firstModification):
-                newPage=output.new_page(len(output))
-                textLength = fitz.get_text_length(filename[:30], fontname="helv", fontsize=fontsize)
-                newPage.insert_text(fitz.Point((newPage.mediabox.width-textLength)/2,(newPage.mediabox.height-textLength)/2),filename[:30],fontsize=fontsize)
-                firstModification=False
-            if(page.number not in pagesKeyword):
-                pagesKeyword.append(page.number)
-                page.clean_contents()
-                output.insert_pdf(doc, from_page=page.number, to_page=page.number)
-                outputModified=True
-                watermark="{filename}-page {pageNumber}".format(filename=filename[:30],pageNumber=page.number+1)
-                output[len(output)-1].insert_text((fitz.get_text_length(watermark, fontname="helv", fontsize=5)/2, 10), watermark)
-    if(outputCreated):
-        output.save(bot.output+"/BOT_"+bot.query+"_"+date+".pdf")
-        output.close()
-    elif(outputModified):
-        output.save(bot.output+"/BOT_"+bot.query+"_"+date+".pdf",incremental=True,encryption=0)
-        output.close()
+    try:
+        for page in doc:
+            page_text = page.get_text()
+            lines = page_text.split('\n')
+            sanitized_lines = [line for line in lines if not line.strip().isdigit()]
+            sanitized_text = '\n'.join(sanitized_lines)
+            res += sanitized_text
+            if(len(findKeyword(listKeywords,sanitized_text,search))!=0):
+                if(not outputExists):
+                    output=fitz.open()
+                    outputCreated=True
+                    outputExists=True
+                if(firstModification):
+                    newPage=output.new_page(len(output))
+                    textLength = fitz.get_text_length(filename[:30], fontname="helv", fontsize=fontsize)
+                    newPage.insert_text(fitz.Point((newPage.mediabox.width-textLength)/2,(newPage.mediabox.height-textLength)/2),filename[:30],fontsize=fontsize)
+                    firstModification=False
+                if(page.number not in pagesKeyword):
+                    pagesKeyword.append(page.number)
+                    page.clean_contents()
+                    output.insert_pdf(doc, from_page=page.number, to_page=page.number)
+                    outputModified=True
+                    watermark="{filename}-page {pageNumber}".format(filename=filename[:30],pageNumber=page.number+1)
+                    output[len(output)-1].insert_text((fitz.get_text_length(watermark, fontname="helv", fontsize=5)/2, 10), watermark)
+        if(outputCreated):
+            output.save(bot.output+"/BOT_"+bot.query+"_"+date+".pdf")
+            output.close()
+        elif(outputModified):
+            output.save(bot.output+"/BOT_"+bot.query+"_"+date+".pdf",incremental=True,encryption=0)
+            output.close()
+    except: 
+        print(f"File at {pdf_file_path} is encrypted")
+        return ""
     doc.close()
     return re.sub(r"\n(?![A-Z])", "", res)
 
